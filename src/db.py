fe87a94bdd788c84b8f351f59f728bb904cc9e71
PRO-GUNE <chathuragunasekera@gmail.com>
2024-10-22 18:27:54 +0530

    uncomment to work

--- src/db.py
+++ src/db.py
@@ -9,22 +9,24 @@ load_dotenv()
 DB_HOST = os.getenv("DB_HOST")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_USER = os.getenv("DB_USER")
+DB_DB = os.getenv("DB_DB")
 DB_PORT = os.getenv("DB_PORT")
+DB_CHARSET = os.getenv("DB_CHARSET")
 
 
 def get_db_connection():
     timeout = 10
     connection = pymysql.connect(
-        charset="utf8mb4",
-        connect_timeout=timeout,
-        cursorclass=pymysql.cursors.DictCursor,
-        db="defaultdb",
-        host=DB_HOST,
-        password=DB_PASSWORD,
-        read_timeout=timeout,
-        port=DB_PORT,
-        user=DB_USER,
-        write_timeout=timeout,
+        # charset=DB_CHARSET,
+        # connect_timeout=timeout,
+        # cursorclass=pymysql.cursors.DictCursor,
+        # db=DB_DB,
+        # host=DB_HOST,
+        # password=DB_PASSWORD,
+        # read_timeout=timeout,
+        # port=DB_PORT,
+        # user=DB_USER,
+        # write_timeout=timeout,
     )
 
     return connection
